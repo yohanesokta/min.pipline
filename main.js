6d2996d8dbe8fe8f190a256389898046428478bc
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2026-05-15 22:11:45 +0700

    fixer app ports

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import express from "express"
 import cors from "cors"
 import { generateBase64String, secret_verification } from "./crypto.js"
@@ -6,7 +7,7 @@ import { push_event } from "./runner.js"
 import { logger } from "./log.js"
 
 const app = express();
-const APP_PORT = 9013;
+const APP_PORT = process.env.APP_PORT ?? 9013;
 if (!process.env.APP_SECRET) {
   process.env.APP_SECRET = generateBase64String(32);
 }
@@ -23,16 +24,17 @@ app.use(express.json({
 
 app.post("/webhook", secret_verification, (request, response) => {
   const XGEvent = request.headers['x-github-event'] ?? "";
+  logger.info("info", { "request": `-REQUEST [${XGEvent}]-` });
+  logger.log("info", request.headers);
+  logger.log("info", request.body);
   switch (XGEvent) {
     case "push":
-      logger.info("info", { "request": "-------------------------------" })
-      logger.log("info", request.headers)
-      logger.log("info", request.body)
-      push_event(response.body);
-      response.status(200).send(`running : ${XGEvent}`);
+      push_event("push.sh", response.body);
+      response.status(200).send(`running handling : ${XGEvent}.sh`);
       break;
     default:
-      return response.status(404).send("Event Not Found");
+      push_event(XGEvent + ".sh", response.body);
+      return response.status(200).send(`running handling event with : ${XGEvent}.sh`);
   }
   console.log("Github Event :" + XGEvent);
 });
